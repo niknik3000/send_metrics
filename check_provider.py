6b0f739f8ee7f2b1ad6cf7848b56435492fdb098
volkovna <na.volkov@tensor.ru>
2023-02-20 12:45:24 +0300

    INFO

--- check_provider.py
+++ check_provider.py
@@ -1,16 +1,17 @@
 import logging
 import random
-import requests
 import time
 
+import requests
+
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='[%(asctime)s][%(levelname)s] %(message)s',
     datefmt='%d-%b-%y %H:%M:%S',
     handlers=[
-            logging.FileHandler("check_connect.log"),
-            logging.StreamHandler()
-        ]
+        logging.FileHandler("check_connect.log"),
+        logging.StreamHandler()
+    ]
 )
 
 hub = [
@@ -27,11 +28,14 @@ hub = [
 
 def check_connect(host):
     try:
-        get_data = requests.get(host, timeout=5)
+        get_data = requests.get(host, timeout=1)
         if get_data.status_code != 200:
-            logging.error(f"Не достучались до узла {host}, код {get_data.status_code}")
+            logging.error(
+                f"Не достучались до узла {host}, код {get_data.status_code}\nЗаголовки ответа: {get_data.headers}")
     except Exception as ex:
         logging.critical(f"При попытке доступа к {host} критическая ошибка: {ex}")
+    else:
+        logging.info(f"{host} -> {get_data.reason}")
 
 
 if __name__ == "__main__":
