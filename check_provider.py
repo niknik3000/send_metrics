3486b68164812d5dea9503cf2a795ef5464a324e
volkovna <na.volkov@tensor.ru>
2023-02-20 11:48:31 +0300

    fix

--- check_provider.py
+++ check_provider.py
@@ -1,5 +1,10 @@
-import requests, time, logging, os, random, datetime
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s][%(levelname)s] %(message)s', datefmt='%d-%b-%y %H:%M:%S', filename="check_connect.log")
+import logging
+import random
+import requests
+import time
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s][%(levelname)s] %(message)s', datefmt='%d-%b-%y %H:%M:%S',
+                    filename="check_connect.log")
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
 
@@ -12,18 +17,19 @@ hub = [
     "https://packages.microsoft.com"
 ]
 
+
 def check_connect(host):
     try:
-        get_data = requests.get(host)
+        get_data = requests.get(host, timeout=5)
         if get_data.status_code != 200:
             logging.error(f"Не достучались до узла {host}")
     except Exception as ex:
         logging.critical(f"При попытке достучаться до узла {host} критическая ошибка: {ex}")
-    logging.info(f"Опрос узла {host} завершилася со статусом {get_data.status_code}")
-    
+    else:
+        logging.info(f"Опрос узла {host} завершился со статусом {get_data.status_code}")
+
 
 if __name__ == "__main__":
     while True:
-        for h in hub:
-            check_connect(h)
-        time.sleep(5)
+        check_connect(hub[random.randrange(0, len(hub) - 1)])
+        time.sleep(2)
